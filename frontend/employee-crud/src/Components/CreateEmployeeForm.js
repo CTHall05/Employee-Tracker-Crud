0e3058ce9bbc6ac55d526b2b8d03bc97e0e281fb
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-05-29 14:55:46 +1000

    Add employee form design completed

--- frontend/employee-crud/src/Components/CreateEmployeeForm.js
+++ frontend/employee-crud/src/Components/CreateEmployeeForm.js
@@ -28,59 +28,63 @@ const CreateEmployeeForm = ({ onCreate }) => {
   };
   
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        First Name:
-        <input 
-          type="text"
-          name="first_name"
-          value={employeeData.first_name}
-          onChange={handleChange}
-          required 
-        />
-      </label>
-      <label>
-        Last Name:
-        <input 
-          type="text"
-          name="last_name"
-          value={employeeData.last_name}
-          onChange={handleChange}
-          required 
-        />
-      </label>
-      <label>
-        Age:
-        <input 
-          type="text"
-          name="age"
-          value={employeeData.age}
-          onChange={handleChange}
-          required 
-        />
-      </label>
-      <label>
-        Years of Employment:
-        <input 
-          type="text"
-          name="years_of_employment"
-          value={employeeData.years_of_employment}
-          onChange={handleChange}
-          required 
-        />
-      </label>
-      <label>
-        Role:
-        <input 
-          type="text"
-          name="role"
-          value={employeeData.role}
-          onChange={handleChange}
-          required 
-        />
-      </label>
+    <form className='create_employee_form' onSubmit={handleSubmit}>
+        <label className='form_label_container'>
+          First Name:
+          <input 
+            type="text"
+            name="first_name"
+            value={employeeData.first_name}
+            onChange={handleChange}
+            required 
+          />
+        </label>
+      
 
-      <button type="submit">Submit</button>
+        <label className='form_label_container'>
+          Last Name:
+          <input 
+            type="text"
+            name="last_name"
+            value={employeeData.last_name}
+            onChange={handleChange}
+            required 
+          />
+        </label>
+
+        <label className='form_label_container'>
+          Age:
+          <input 
+            type="text"
+            name="age"
+            value={employeeData.age}
+            onChange={handleChange}
+            required 
+          />
+        </label>
+
+        <label className='form_label_container'>
+          Years of Employment:
+          <input 
+            type="text"
+            name="years_of_employment"
+            value={employeeData.years_of_employment}
+            onChange={handleChange}
+            required 
+          />
+        </label>
+
+        <label className='form_label_container'>
+          Role:
+          <input 
+            type="text"
+            name="role"
+            value={employeeData.role}
+            onChange={handleChange}
+            required 
+          />
+        </label>
+        <button className="button-design"type="submit">Submit</button>
     </form>
   );
 };
